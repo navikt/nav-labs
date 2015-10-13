c52e8833f94d9d50fcb9bcf84bbe7ba613570b0a
Helge Fredheim <helge.fredheim@bekk.no>
2015-10-13 09:37:16 +0200

    Div. tilpasninger før visningsmøte

--- src/main/resources/site/pages/page-html/page-html.js
+++ src/main/resources/site/pages/page-html/page-html.js
@@ -6,17 +6,29 @@ exports.get = function(req) {
   var view = resolve('page-html.html');
 
   var content = portal.getContent(); 
+  var site = portal.getSite(); 
+  var siteConfig = site.data.siteConfig.config; 
 
   var headerRegion = content.page.regions.header;
   var mainRegion = content.page.regions.main;
   var footerRegion = content.page.regions.footer;
 
-  var model = {
+  var siteTitle = siteConfig.siteTitle;
+  var siteUrl = portal.pageUrl({
+    path: site._path,
+    type: "absolute"
+  });
+
+  var model = { 
       mainRegion: mainRegion,
       headerRegion: headerRegion, 
       footerRegion: footerRegion,
-      pageTitle: content.displayName ? content.displayName + ' | ' + "Nav Labs" : "Nav Labs",
-      language: content.language || "no"
+      footerLinks: siteConfig.footerLinks,
+      pageTitle: content.displayName ? content.displayName + ' | ' + siteTitle : siteTitle,
+      language: content.language || "no",
+      siteTitle: siteTitle,
+      siteUrl: siteUrl,
+      departmentNameAndYear: siteConfig.departmentName || "Arbeids- og velferdsetaten" + " " + new Date().getFullYear()
   };
 
   var body = thymeleaf.render(view, model);
