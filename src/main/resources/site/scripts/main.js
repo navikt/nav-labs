1cce1357f322d0ab7ef05c56150afed226aefc5c
Helge Fredheim <helge.fredheim@bekk.no>
2015-10-14 16:03:11 +0200

    Fjerner avhengighet til browserify, da det ikke trengs foreløpig. Skriver heller vanilla JS.

--- src/main/resources/site/scripts/main.js
+++ src/main/resources/site/scripts/main.js
@@ -1,4 +1,33 @@
-var share = require("./shareSocialMedia.js");
-var $ = require("./vendor/jquery.js");
+(function() {
 
-share.init($(".js-share"));
+	var openWindow = function(e) {
+
+		if(e && e.preventDefault) {
+			e.preventDefault()
+		}
+
+		var windowWidth = document.documentElement.clientWidth;
+		var windowHeight = document.documentElement.clientHeight;
+
+	    var width  = 575,
+	        height = 400,
+	        left   = (windowWidth  - width)  / 2,
+	        top    = (windowHeight - height) / 2,
+	        url    = this.href,
+	        title  = this.innerHTML, 
+	        opts   = 'status=1' +
+	                 ',width='  + width  +
+	                 ',height=' + height +
+	                 ',top='    + top    +
+	                 ',left='   + left;
+	    
+	    window.open(url, title, opts);		
+	};	
+
+	var socialMediaElements = document.getElementsByClassName("js-share");
+
+	for(var i = 0; i < socialMediaElements.length; i++) {
+		socialMediaElements[i].addEventListener("click", openWindow);
+	}
+
+})(); 
