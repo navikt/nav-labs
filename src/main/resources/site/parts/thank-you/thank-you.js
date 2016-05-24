e304318b7a4b23dac9e9928b1f24a2cde668d15d
Helge Fredheim <helge.fredheim@bekk.no>
2016-05-24 12:08:17 +0200

    La til knapp for å gå videre

--- src/main/resources/site/parts/thank-you/thank-you.js
+++ src/main/resources/site/parts/thank-you/thank-you.js
@@ -1,17 +1,20 @@
 var portal = require('/lib/xp/portal');
 var thymeleaf = require('/lib/xp/thymeleaf');
-var content = require('/lib/xp/content');
 var utils = require("/lib/utilities.js");
-var TestModel = require("/lib/TestModel.js");
 
 exports.get = function() {
 
   var view = resolve('thank-you.html');
-  var model = portal.getContent();
+  var contentModel = portal.getContent();
   var siteConfig = portal.getSiteConfig(); 
 
-  model.displayName = model.displayName === "" ? "Tittel kommer her" : model.displayName;
-  model.data.body = model.data.body || siteConfig.defualtThankYouText || "<p>Tekst kommer her.</p>";
+  var model = {};
+  model.displayName = contentModel.displayName === "" ? "Tittel kommer her" : contentModel.displayName;
+  model.body = contentModel.data.body || siteConfig.defualtThankYouText || "<p>Tekst kommer her.</p>";
+  model.displayNext = contentModel.data.nextUrl && contentModel.data.nextLabel && contentModel.data.nextUrl.trim() !== "" && contentModel.data.nextLabel.trim() !== "";
+  model.nextUrl = contentModel.data.nextUrl;
+  model.nextLabel = contentModel.data.nextLabel;
+
   var body = thymeleaf.render(view, model);
 
   return {
